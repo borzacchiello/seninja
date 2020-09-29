c6533912228c693c815c62c50ff42eb323e74894
luca borzacchiello <lucaborza@gmail.com>
2020-09-29 15:46:43 +0200

    changed command names

--- __init__.py
+++ __init__.py
@@ -45,72 +45,72 @@ from .apis import (
 )
 
 PluginCommand.register_for_address(
-    "SENinja\\0 - Start symbolic execution",
+    "SENinja\\0   Start symbolic execution",
     "create the first state for symbolic execution at current address",
     _async_start_se
 )
 PluginCommand.register_for_address(
-    "SENinja\\1 - Change current state",
+    "SENinja\\1   Change current state",
     "change current state with the deferred one at current address (if any)",
     _async_change_current_state
 )
 PluginCommand.register(
-    "SENinja\\2 - Step",
+    "SENinja\\2   Step",
     "execute one instruction with the current state",
     _async_step
 )
-PluginCommand.register(
-    "SENinja\\3 - Continue until branch",
-    "execute instructions in the current state until a fork occurs",
-    _async_continue_until_branch
-)
 PluginCommand.register_for_address(
-    "SENinja\\4 - Continue until address",
-    "execute instructions in the current state until the currently selected address is reached",
-    _async_continue_until_address
-)
-PluginCommand.register_for_address(
-    "SENinja\\5 - Merge states",
+    "SENinja\\3   Merge states",
     "merge all states at current address in one state",
     _async_merge_states
 )
 PluginCommand.register(
-    "SENinja\\6 - Save active state",
+    "SENinja\\4   Save active state",
     "save active state in deferred queue",
     _async_save_active_state
 )
 PluginCommand.register_for_address(
-    "SENinja\\7 - Set IP to address",
+    "SENinja\\5   Set IP to address",
     "set ip of active state to current address",
     _async_change_active_state_ip
 )
+PluginCommand.register(
+    "SENinja\\6   Run\\1   Continue until branch",
+    "execute instructions in the current state until a fork occurs",
+    _async_continue_until_branch
+)
 PluginCommand.register_for_address(
-    "SENinja\\8 - Run\\0 - Set target",
+    "SENinja\\6   Run\\2   Continue until address",
+    "execute instructions in the current state until the currently selected address is reached",
+    _async_continue_until_address
+)
+PluginCommand.register_for_address(
+    "SENinja\\6   Run\\3   Set searcher target",
     "set run target",
     _set_run_target
 )
 PluginCommand.register_for_address(
-    "SENinja\\8 - Run\\1 - Set avoid",
+    "SENinja\\6   Run\\4   Set searcher avoid",
     "set run avoid",
     _set_run_avoid
 )
 PluginCommand.register(
-    "SENinja\\8 - Run\\2 - Run (DFS)",
+    "SENinja\\6   Run\\5   Run (DFS)",
     "run (target must be set)",
     _async_run_dfs_searcher
 )
 PluginCommand.register(
-    "SENinja\\8 - Run\\3 - Run (DFS) findall",
+    "SENinja\\6   Run\\6   Run (DFS) findall",
     "run (target must be set)",
     _async_run_dfs_searcher_findall
 )
 PluginCommand.register(
-    "SENinja\\8 - Run\\4 - Run (BFS)",
+    "SENinja\\6   Run\\7   Run (BFS)",
     "run (target must be set)",
     _async_run_bfs_searcher
 )
 PluginCommand.register(
-    "SENinja\\9 - Reset symbolic execution",
+    "SENinja\\7   Reset",
     "delete all states",
     _async_reset_se
 )
